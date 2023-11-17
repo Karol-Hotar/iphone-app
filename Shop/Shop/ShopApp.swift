459361a94b32e91c291c44deec02a59568859d93
Karol Hotár <hotar.karol@gmail.com>
2023-11-17 18:15:23 +0100

    First Commit

---
+++ Shop/Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Karol Hotar on 17.11.23.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
